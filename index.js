907a74b971527f8e87d5689e89aaf2eb683aabfa
Sensational Code <sensational.code@gmail.com>
2020-09-15 21:51:09 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const observerConfig = { attributes: true, childList: true, characterData: true };
+
+// Wait for the news container element to load
+const loadObserver = (mutations, observer) => {
+	const newsContainer = document.querySelector('[data-testid="Portfolio-NewsSection"]');
+
+	if (newsContainer) {
+		handleLoad(newsContainer);
+		observer.disconnect();
+	}
+}
+
+// Add the load observer
+new MutationObserver(loadObserver).observe(document.body, observerConfig);
+
+const handleLoad = (newsContainer) => {
+	// Watch for the "show newer articles" button to appear and click it
+	const newsObserver = mutations => {
+		mutations.forEach(mutation => {
+			mutation.addedNodes[0].firstChild.click();
+		});
+	}
+
+	// Add the news container observer
+	new MutationObserver(newsObserver).observe(newsContainer, observerConfig);
+}
